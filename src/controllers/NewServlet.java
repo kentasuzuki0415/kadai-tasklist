dba29400a7a0d4a411ae39578b40dfd71b6f3531
kentasuzuki <kakasikakasi0415@gmail.com>
2018-09-16 22:02:25 +0900

    Add CreateServlet.java

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -36,7 +36,7 @@ public class NewServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
         request.setAttribute("task", new Task());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
         rd.forward(request, response);
 
 
